0c6bb6f13c3a00a3e73968b9df85510fc37ff25f
AshrafIbrahim03 <btd2eb@virginia.edu>
2023-11-16 10:05:10 -0500

    Added roversa pin mappings to file

---
+++ source/Pins.h
@@ -0,0 +1,24 @@
+
+//Pin definition from finchblox
+#define RESET_PIN 2 // Pin to reset/turn off Finch - micro:bit pin 1
+
+
+/*
+These pin definitions are from ELECTRONICS/pinoutRobot2.1.pdf from the Roversa Drive
+*/
+
+#define I2CSDA_PIN 20
+#define I2CSCL_PIN 19
+
+#define LEFT_BUTTON_PIN 16
+#define RIGHT_BUTTON_PIN 15
+#define REVERSE_BUTTON_PIN 14
+#define FORWARD_BUTTON_PIN 13
+#define STOP_BUTTON_PIN 9
+#define PLAY_BUTTON_PIN 8
+
+#define RIGHT_SERVO_PIN 2
+#define LEFT_SERVO_PIN 1
+
+
+#define GVS_SIGNAL_PIN 0
