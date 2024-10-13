e2e98b681f3fa90f6f3f4f168ccf1faf3452a026
Ashraf Ibrahim <btd2eb@virginia.edu>
2024-10-13 19:19:58 -0400

    DOES NOT WORK, added basic I2c

--- source/main.cpp
+++ source/main.cpp
@@ -11,8 +11,10 @@
 #include "Pins.h"
 #include "Notes.h"
 #include "BBMicroBit.h"
+#include "MicroBitI2C.h"
 
 MicroBit uBit;
+// MicroBitI2C ledI2c = MicroBitI2C(I2C_SDA0, I2C_SCL0);
 
 // Check for BLE data, execute appropriate commands
 // sensor data is sent asynchronously in a different fiber
@@ -126,8 +128,14 @@ int main()
 
     // Wait for the SAMD bootloader checker
     fiber_sleep(1850);
-     // Figure out what we are called, start flashing our initials
+    // Figure out what we are called, start flashing our initials
     getInitials_fancyName();
+
+    // uint8_t on = 0xff;
+    // uint8_t off = 0x00;
+    // ledI2c.write(0x03, &on, (unsigned int)1);
+    // fiber_sleep(99);
+    // ledI2c.write(0x03, &off, (unsigned int)1);
 
     // Get our name prefix - BB, FN, or MB - depending on what we are attached to
     ManagedString bbDevName = whichDevice();
